fae7f253d1062daaf125767dd59b0de16170dfdd
RamilHinshaw <RamilHinshaw@gmail.com>
2022-07-30 14:34:14 -0500

    updated initial

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,7 +1,8 @@
 extends KinematicBody
 
 
-var motion = Vector3.ZERO
+export var acceleration:float = 512
+export var motion:Vector3 = Vector3.ZERO
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -13,7 +14,6 @@ func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
-	motion.x = 1
 	move_and_slide(motion, Vector3.UP)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
